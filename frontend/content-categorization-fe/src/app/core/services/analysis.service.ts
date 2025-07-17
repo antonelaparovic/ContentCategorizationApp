aff4f979edecbdf7c923c86df2c0f591ea3a1d1f
antonelaparovic <antonelaparovic11@gmail.com>
2025-07-18 01:40:05 +0200

    fix prod

--- frontend/content-categorization-fe/src/app/core/services/analysis.service.ts
+++ frontend/content-categorization-fe/src/app/core/services/analysis.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { AnalysisResponse } from '../../core/dtos/analysis-response';
-import { environment } from '../../environments/environment';
+import { environment } from '../../../environments/environment';
 import { AnalysisRequest } from '../../core/dtos/analysis-request';
 
 @Injectable({ providedIn: 'root' })
