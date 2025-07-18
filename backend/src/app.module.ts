e911947c367c625ebd1e031c9bac883203052d6e
antonelaparovic <antonelaparovic11@gmail.com>
2025-07-18 11:50:30 +0200

    add throttle for rate limiting

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,14 +5,30 @@ import { OpenaiModule } from './openai/openai.module';
 import { AnalysisModule } from './analysis/analysis.module';
 import { ConfigModule } from '@nestjs/config';
 import openaiConfig from './config/openai.config';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [OpenaiModule, AnalysisModule, ConfigModule.forRoot({
     isGlobal: true,
     load: [openaiConfig]
-  })
+  },
+),
+    ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          ttl: 5000,
+          limit: 3,
+        },
+      ],
+    }),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, 
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
